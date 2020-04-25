b1b039d11f72b7180ef51c6d60c19f4b4af979cd
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2020-04-24 23:17:04 -0600

    init commit

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../stylesheets/App.css';
+import Game from './Game';
+
+function App() {
+  return (
+    <div id="app">
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Game.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div id="game">
+        Game
+      </div>
+    );
+  }
+}
+
+export default Game;
